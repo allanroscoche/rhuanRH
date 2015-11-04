6b1fc80edcc943113c98b831948ac38576cd3d55
Allan <allanroscoche@gmail.com>
2015-11-03 23:49:21 -0200

    login errado

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.1.0'
 end
 
+# Sistema de Login
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+locations = Location.create([{uf: 'PR', localidade: 'Paranaguá'},{uf: 'PR', localidade:'Curitiba'}])
